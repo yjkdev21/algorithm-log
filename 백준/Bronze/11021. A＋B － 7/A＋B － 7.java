e278bc564e6ddb32357b409722bd6fe8b757981e
yjkdev21 <yjkdev21@gmail.com>
2025-09-24 17:58:13 +0900

    [Bronze V] Title: A+B - 7, Time: 136 ms, Memory: 16092 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        for(int i=1; i<=n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write("Case #"+i+": "+ (a+b)+"\n");
+        }
+        
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
